1b4d1c36284be2148b75a1d44579d0f8287e45b0
何杨坤 <1017660522@qq.com>
2026-05-28 09:09:24 +0800

    feat:新增模块（common模块、API模块、Auth模块）

---
+++ cloud-common/src/main/java/com/spring/demo/controller/CommonController.java
@@ -0,0 +1,19 @@
+package com.spring.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+    @GetMapping("/api/hello")
+    public String hello(){
+        return "✅ 用户服务 SpringBoot4.0.5 正常运行";
+    }
+
+    @GetMapping("/internal/hello")
+    public String inView(){
+        return "✅ 内部服务，不对外开放 SpringBoot4.0.5 正常运行";
+    }
+
+
+}
